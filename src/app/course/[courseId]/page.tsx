b68f3cc86b4a896db7d3f67de86da1744e87df11
= <mgrishchenko@zuzex.com>
2024-06-27 10:56:51 +0300

    add image meta tag

--- src/app/course/[courseId]/page.tsx
+++ src/app/course/[courseId]/page.tsx
@@ -1,14 +1,19 @@
 import { sanityFetch } from "@/sanity/lib/client"
 import CourseDetail from "@/src/components/ui/CourseDetail/CourseDetail"
+import { generateUrl } from "@/src/components/utils/generateUrl"
 import { SanityDocument } from "next-sanity"
 
 export const generateMetadata = async ({params} : {params:{courseId: string}}) => {
+    const data = await sanityFetch<SanityDocument>({query: `*[ _type == 'course' && nativeId == ${params.courseId} ]`})
+    // const {image} = data[0]
+    const url = generateUrl(data[0].assets._ref).url()
     const product = await sanityFetch<SanityDocument[]>({query: `*[ _type == 'course' && nativeId == ${params.courseId} ] {metaKeywords, metaTitle, metaDescription }`})
     const { metaKeywords, metaTitle, metaDescription } = product[0];
     return{
         title: metaDescription,
         description: metaTitle,
-        keywords: metaKeywords
+        keywords: metaKeywords,
+        images: url
     }
 }
 
