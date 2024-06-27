ea25ecf7bbab7e377bdd729a809ddebe92e5bd94
= <mgrishchenko@zuzex.com>
2024-06-27 11:05:44 +0300

    fix og meta

--- src/app/course/[courseId]/page.tsx
+++ src/app/course/[courseId]/page.tsx
@@ -4,16 +4,19 @@ import { generateUrl } from "@/src/components/utils/generateUrl"
 import { SanityDocument } from "next-sanity"
 
 export const generateMetadata = async ({params} : {params:{courseId: string}}) => {
-    const data = await sanityFetch<SanityDocument>({query: `*[ _type == 'course' && nativeId == ${params.courseId} ]`})
-    // const {image} = data[0]
-    const url = generateUrl(data[0].assets._ref).url()
-    const product = await sanityFetch<SanityDocument[]>({query: `*[ _type == 'course' && nativeId == ${params.courseId} ] {metaKeywords, metaTitle, metaDescription }`})
-    const { metaKeywords, metaTitle, metaDescription } = product[0];
+    const product = await sanityFetch<SanityDocument[]>({query: `*[ _type == 'course' && nativeId == ${params.courseId} ] {metaKeywords, metaTitle, metaDescription, image }`})
+    const { metaKeywords, metaTitle, metaDescription, image } = product[0];
+    const url = generateUrl(image.asset._ref).url()
     return{
         title: metaDescription,
         description: metaTitle,
         keywords: metaKeywords,
-        images: url
+        openGraph:{
+            images: url,
+            title: metaTitle,
+            description: metaDescription,
+            keywords: metaKeywords
+        }
     }
 }
 
